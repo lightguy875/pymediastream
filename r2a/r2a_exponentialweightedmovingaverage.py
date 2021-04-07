35fdd8587ac28d4d94b1974e08556d95342626ed
lightguy875 <luz875@gmail.com>
2021-04-07 12:34:00 -0300

    Algoritmo adaptado para versão de pandas

--- r2a/r2a_exponentialweightedmovingaverage.py
+++ r2a/r2a_exponentialweightedmovingaverage.py
@@ -6,12 +6,18 @@ from pandas import *
 
 class R2A_ExponentialWeightedMovingAverage(IR2A):
 
+
     def __init__(self, id):
         IR2A.__init__(self, id)
         self.throughputs = []
         self.request_time = 0
         self.qi = []
-        self.smooth = []
+        self.smooth = [0]
+        self.estimateband = [0]
+        self.k = 0.56
+        self.w = 0.3
+        self.alfa = 0.2
+        self.t = []
 
     def handle_xml_request(self, msg):
         self.request_time = time.perf_counter()
@@ -22,28 +28,43 @@ class R2A_ExponentialWeightedMovingAverage(IR2A):
         parsed_mpd = parse_mpd(msg.get_payload())
         self.qi = parsed_mpd.get_qi()
 
-        t = time.perf_counter() - self.request_time
-        self.throughputs.append(msg.get_bit_length() / (1.5*t))
-
+        self.t.append(time.perf_counter() - self.request_time) 
+        self.throughputs.append(msg.get_bit_length() / self.t[-1])
+        if self.estimateband[-1] != 0:
+            self.estimateband.append((self.k*(self.w - (self.estimateband[-1]-self.throughputs[-1] + self.w))*self.t[-1]) + self.estimateband[-1])
+        else:
+            self.estimateband.append(self.throughputs[-1])
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
         self.request_time = time.perf_counter()
-        df = DataFrame (self.throughputs,columns=['throughputs'])
-        self.smooth = df.ewm(alpha=0.5).mean()
-        self.smooth = df.values.tolist()
+        df = DataFrame (self.estimateband,columns=['throughputs'])
+        df.ewm(alpha=0.5).mean()
+        # self.smooth.append(df.values.tolist()[-1][-1]) 
 
+        if self.smooth[-1] != 0:
+            self.smooth.append((-self.alfa*(df.values.tolist()[-1][-1] - self.estimateband[-1])*self.t[-1]) + df.values.tolist()[-1][-1])
+        else: 
+            self.smooth.append(df.values.tolist()[-1][-1]) 
+        
+        print(self.smooth[-1])
         selected_qi = self.qi[0]
         for i in self.qi:
-            if self.smooth[-1][-1] > i:
+            if self.smooth[-1] > i:
                 selected_qi = i
 
         msg.add_quality_id(selected_qi)
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
-        t = time.perf_counter() - self.request_time
-        self.throughputs.append(msg.get_bit_length() / (1.5*t))
+        self.t.append(time.perf_counter() - self.request_time)
+        self.throughputs.append(msg.get_bit_length() / self.t[-1])
+
+        if self.estimateband[-1] != 0:
+            self.estimateband.append((self.k*(self.w - (self.estimateband[-1]-self.throughputs[-1] + self.w))*self.t[-1]) + self.estimateband[-1])
+        else:
+            self.estimateband.append(self.throughputs[-1])
+
         self.send_up(msg)
 
     def initialize(self):
