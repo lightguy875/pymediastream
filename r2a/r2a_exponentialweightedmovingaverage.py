fcc33f0158d9a2533a215860086682ee2e2546d5
lightguy875 <luz875@gmail.com>
2021-04-11 14:29:43 -0300

    Atualização do algoritmo

--- r2a/r2a_exponentialweightedmovingaverage.py
+++ r2a/r2a_exponentialweightedmovingaverage.py
@@ -17,21 +17,20 @@ class R2A_ExponentialWeightedMovingAverage(IR2A):
         self.k = 0.56
         self.w = 0.3
         self.alfa = 0.2
-        self.t = []
+        self.t = 0
 
     def handle_xml_request(self, msg):
         self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
-
         parsed_mpd = parse_mpd(msg.get_payload())
         self.qi = parsed_mpd.get_qi()
 
-        self.t.append(time.perf_counter() - self.request_time) 
-        self.throughputs.append(msg.get_bit_length() / self.t[-1])
+        self.t = time.perf_counter() - self.request_time
+        self.throughputs.append(msg.get_bit_length() / self.t)
         if self.estimateband[-1] != 0:
-            self.estimateband.append((self.k*(self.w - (self.estimateband[-1]-self.throughputs[-1] + self.w))*self.t[-1]) + self.estimateband[-1])
+            self.estimateband.append(((self.k*(self.w - max(0,(self.estimateband[-1]-self.throughputs[-1] + self.w))))*self.t) + self.estimateband[-1])
         else:
             self.estimateband.append(self.throughputs[-1])
         self.send_up(msg)
@@ -39,14 +38,14 @@ class R2A_ExponentialWeightedMovingAverage(IR2A):
     def handle_segment_size_request(self, msg):
         self.request_time = time.perf_counter()
         df = DataFrame (self.estimateband,columns=['throughputs'])
-        df.ewm(alpha=0.5).mean()
-        # self.smooth.append(df.values.tolist()[-1][-1]) 
+        df.ewm(alpha=0.5,min_periods=5).mean()
+        valor = df.values.tolist()[-1][-1]
 
         if self.smooth[-1] != 0:
-            self.smooth.append((-self.alfa*(df.values.tolist()[-1][-1] - self.estimateband[-1])*self.t[-1]) + df.values.tolist()[-1][-1])
+            self.smooth.append((-self.alfa*(valor - self.estimateband[-1])*self.t) + valor)
         else: 
-            self.smooth.append(df.values.tolist()[-1][-1]) 
-        
+            self.smooth.append(valor) 
+
         selected_qi = self.qi[0]
         for i in self.qi:
             if self.smooth[-1] > i:
@@ -56,11 +55,11 @@ class R2A_ExponentialWeightedMovingAverage(IR2A):
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
-        self.t.append(time.perf_counter() - self.request_time)
-        self.throughputs.append(msg.get_bit_length() / self.t[-1])
+        self.t = time.perf_counter() - self.request_time
+        self.throughputs.append(msg.get_bit_length() / self.t)
 
         if self.estimateband[-1] != 0:
-            self.estimateband.append((self.k*(self.w - (self.estimateband[-1]-self.throughputs[-1] + self.w))*self.t[-1]) + self.estimateband[-1])
+            self.estimateband.append((self.k*(self.w - (self.estimateband[-1]-self.throughputs[-1] + self.w))*self.t) + self.estimateband[-1])
         else:
             self.estimateband.append(self.throughputs[-1])
 
